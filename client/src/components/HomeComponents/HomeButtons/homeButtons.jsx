e5a12c8e2ab08224cc12a9d491dd633d584c8733
saja04 <saajamilf@gmail.com>
2023-11-03 16:00:22 -0300

    finished 'looking for a job' text

--- client/src/components/HomeComponents/HomeButtons/homeButtons.jsx
+++ client/src/components/HomeComponents/HomeButtons/homeButtons.jsx
@@ -4,21 +4,13 @@ import githubLogo from "../../../assets/github.png";
 import henryLogo from "../../../assets/henry.png";
 
 const HomeButtons = () => {
-  const handlePopUp = () => {
-    const newSpan = document.createElement("span");
-    newSpan.textContent = "Estoy buscando empleo!";
-    const button = document.getElementById("myButton");
-    // button.appendChild(newSpan);
-  };
   return (
     <div className={style.mainDiv}>
-      <h4 className={style.jobTitle}>Estoy Buscando Trabajo!</h4>
       <div className={style.buttonsDiv}>
         <button className={style.curriculumButton} id="myButton">
           <a
             href="https://www.linkedin.com/in/federico-saa-4ab74b297/"
             className={style.curriculumLink}
-            onMouseOver={handlePopUp()}
           >
             Curriculum
           </a>
@@ -27,7 +19,6 @@ const HomeButtons = () => {
         <a
           href="https://www.linkedin.com/in/federico-saa-4ab74b297/"
           className={style.mainLinks}
-          onMouseOver={handlePopUp()}
         >
           <img src={linkedinLogo} alt="linkedin" className={style.logos} />
         </a>
@@ -41,6 +32,7 @@ const HomeButtons = () => {
           <img src={henryLogo} alt="soy henry" className={style.logos} />
         </a>
       </div>
+      <h4 className={style.jobTitle}>Estoy Buscando Trabajo!</h4>
     </div>
   );
 };
