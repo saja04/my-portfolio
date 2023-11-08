f28ba945e12c11710e5c1948940b4fc29fd49504
saja04 <saajamilf@gmail.com>
2023-11-07 23:24:08 -0300

    cambia error deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,7 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 
+{
+  "rewrites": [
+    { "source": "/(.*)", "destination": "/" }
+  ]
+}
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
 })
+
