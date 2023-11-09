e1ecfe2a6459c14369cbfa0ccad9b16f42029c1b
saja04 <saajamilf@gmail.com>
2023-11-09 12:39:18 -0300

    finished softskills

--- client/src/components/AboutComponents/AboutInformation/AboutInformation.jsx
+++ client/src/components/AboutComponents/AboutInformation/AboutInformation.jsx
@@ -21,6 +21,21 @@ const AboutInformation = () => {
             <img src={htmlcsspb} alt="node" />
           </div>
         </div>
+        <div className={style.softContainer}>
+          <h2 className={style.subtitleSoft}>
+            Habilidades<span className={style.softText}>Blandas</span>
+          </h2>
+          <div className={style.sAbilitiesContainer}>
+            <h4>Comunicación</h4>
+            <h4>Trabajo En Equipo</h4>
+            <h4>Pensamiento Crítico</h4>
+            <h4>Gestión de crisis</h4>
+            <h4>Colaborativo</h4>
+          </div>
+        </div>
+        <div className={style.expContainer}>
+          <h2 className={style.subtitleExp}>Experiencia <span className={style.expText}>Pasada</span></h2>
+        </div>
       </div>
     </>
   );
