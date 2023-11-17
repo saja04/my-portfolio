9d7f7646e7012fe61178c7e07029c6b81321723c
saja04 <saajamilf@gmail.com>
2023-11-17 19:15:51 -0300

    the portfolio preview on the about, ABOUT FINISHED

--- client/src/components/AboutComponents/PortfolioPreview/PortfolioPreview.jsx
+++ client/src/components/AboutComponents/PortfolioPreview/PortfolioPreview.jsx
@@ -1,6 +1,8 @@
 import style from "./PortfolioPreview.module.css";
 import codeCar from "../../../assets/codecar.png";
 import pokemon from "../../../assets/pokemon.png";
+import arrow from "../../../assets/arrow1.png";
+import { NavLink } from "react-router-dom";
 
 const PortfolioPreview = () => {
   return (
@@ -8,21 +10,33 @@ const PortfolioPreview = () => {
       <h2 className={style.expTitle}>
         Experiencia <span className={style.expTitle2}>Pasada</span>
       </h2>
-      
+
       <div className={style.cardsContainer}>
-        <div className={style.codeCarContainer}>
+        <h2 className={style.goToTitle}>
+          Ir al <span className={style.title2}>portfolio!</span>
+        </h2>
+
+        <div className={style.imagesContainer}>
+          <span className={style.carCodeText}>carCode</span>
           <img
             src={codeCar}
             alt="federico saa portfolio"
             className={style.codeCarImg}
           />
-        </div>
-        <div className={style.pokemonContainer}>
+          <span className={style.pokemonText}>The Pokedex</span>
           <img
             src={pokemon}
             alt="federico saa portfolio"
             className={style.pokemonImg}
           />
+        </div>
+        <div className={style.goToPortContainer}>
+          <img
+            src={arrow}
+            alt="federico saa portfolio"
+            className={style.goToArrow}
+          />
+          <NavLink to="/portfolio" className={style.portfolioNavLink} />
         </div>
       </div>
     </div>
