b66f0d699bb74b52e32afb8c897eec1184f2b447
saja04 <saajamilf@gmail.com>
2023-12-25 21:37:12 -0300

    trying images as urls

--- client/src/components/HomeComponents/ProfileImage/profileImage.jsx
+++ client/src/components/HomeComponents/ProfileImage/profileImage.jsx
@@ -5,9 +5,9 @@ const ProfileImage = () => {
 
     return(
         <div className={style.mainDiv}>
-            <img src={profileImage} alt="federico saa" className={style.profileImage1} />
-            <img src={profileImage} alt="federico saa" className={style.profileImage2} />
-            <img src={profileImage} alt="federico saa" className={style.profileImage3} />
+            <img src='https://cdn.discordapp.com/attachments/1080681344264314920/1189003468761989141/edit.png?ex=659c94c5&is=658a1fc5&hm=ea6002d986e293e7cbbebad112734c9faab2b13e980dcf944871b5c16bc649ea&' alt="federico saa" className={style.profileImage1} />
+            <img src='https://cdn.discordapp.com/attachments/1080681344264314920/1189003468761989141/edit.png?ex=659c94c5&is=658a1fc5&hm=ea6002d986e293e7cbbebad112734c9faab2b13e980dcf944871b5c16bc649ea&' alt="federico saa" className={style.profileImage2} />
+            <img src='https://cdn.discordapp.com/attachments/1080681344264314920/1189003468761989141/edit.png?ex=659c94c5&is=658a1fc5&hm=ea6002d986e293e7cbbebad112734c9faab2b13e980dcf944871b5c16bc649ea&' alt="federico saa" className={style.profileImage3} />
 
         </div>
     )
